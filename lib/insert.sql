08cd4e639205b0e179d9deff4c3986708272717b
Jakob Persons <jakobep24@gmail.com>
2020-11-25 11:22:52 -0500

    inserting users in insert

---
+++ lib/insert.sql
@@ -0,0 +1,33 @@
+-- INSERT INTO groceries VALUES (1, "Apples", 2, 1);
+-- INSERT INTO groceries VALUES (2, "Oranges", 4, 2);
+-- INSERT INTO groceries VALUES(3, "Peaches", 6, 3);
+
+-- CREATE TABLE users(
+--     id INTEGER PRIMARY KEY,
+--     name TEXT,
+--     age INTEGER
+-- );
+
+-- has 20 users
+
+INSERT INTO users VALUES (id, name, age)
+INSERT INTO users VALUES (1, name, age)
+INSERT INTO users VALUES (2, name, age)
+INSERT INTO users VALUES (3, name, age)
+INSERT INTO users VALUES (4, name, age)
+INSERT INTO users VALUES (5, name, age)
+INSERT INTO users VALUES (6, name, age)
+INSERT INTO users VALUES (7, name, age)
+INSERT INTO users VALUES (8, name, age)
+INSERT INTO users VALUES (9, name, age)
+INSERT INTO users VALUES (10, name, age)
+INSERT INTO users VALUES (11, name, age)
+INSERT INTO users VALUES (12, name, age)
+INSERT INTO users VALUES (13, name, age)
+INSERT INTO users VALUES (14, name, age)
+INSERT INTO users VALUES (15, name, age) 
+INSERT INTO users VALUES (16, name, age)
+INSERT INTO users VALUES (17, name, age)
+INSERT INTO users VALUES (18, name, age)
+INSERT INTO users VALUES (19, name, age)
+INSERT INTO users VALUES (20, name, age)
